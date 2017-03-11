570d1ed53aad4764a628a1b8df2a03913457268b
Zach Bird <zachthebird@gmail.com>
2017-03-10 19:09:48 -0600

    finishing up

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -1,58 +1,67 @@
-
-
+$(document).on('ready', function(){
 // Initialize Firebase
-var config = {
-apiKey: "AIzaSyCiA70aYjLH8jy7eh-0DCg2vk5j5y8RnCU",
-authDomain: "zbirds-1st-project.firebaseapp.com",
-databaseURL: "https://zbirds-1st-project.firebaseio.com",
-storageBucket: "zbirds-1st-project.appspot.com",
-messagingSenderId: "642238249348"
-};
+	var config = {
+	apiKey: "AIzaSyCiA70aYjLH8jy7eh-0DCg2vk5j5y8RnCU",
+	authDomain: "zbirds-1st-project.firebaseapp.com",
+	databaseURL: "https://zbirds-1st-project.firebaseio.com",
+	storageBucket: "zbirds-1st-project.appspot.com",
+	messagingSenderId: "642238249348"
+	};
 
-firebase.initializeApp(config);
+	firebase.initializeApp(config);
 
-var database = firebase.database
-var trainName;
-var destination;
-var firstTrainTime;
-var frequency;
-var nextArrival;
-var minutesAway;
+	var database = firebase.database
+	var trainName;
+	var destination;
+	var firstTrainTime;
+	var firstTrainTimeConverted;
+	var frequency;
+	var nextTrainTime;
+	var currentTime;  
+	var diffTime;
+	var tRemainder;
+	var tMinutesTillTrain;
+	var nextTrain;
 
+	$(document).on('click', '#submit', function(){
 
+		trainName = $('#trainNameInput').val().trim();
+		destination = $('#destinationInput').val().trim();
+		frequency = $('#frequencyInput').val().trim();
+		firstTrainTime = $('#firstTrainTimeInput').val().trim();
+		firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+		currentTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
+		diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+		tRemainder = diffTime % frequency;
+		tMinutesTillTrain = frequency - tRemainder;
+		nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm a");
 
-$(document).on('click', '#submit', function(){
-	trainName = $('#trainNameInput').val().trim();
-	destination = $('#destinationInput').val().trim();
-	frequency = $('#frequencyInput').val().trim();
-	firstTrainTime = $('#firstTrainTimeInput').val().trim();
-	console.log(trainName, destination, firstTrainTime, frequency);
+		firebase.database().ref().push({
+			trainName: trainName,
+			destination: destination,
+			firstTrainTime: firstTrainTime,
+			frequency: frequency,
+			usersCurrentTime: currentTime,
+		});
+		
+		$('#trainNameInput').val('');
+		$('#destinationInput').val('');
+		$('#firstTrainTimeInput').val('');
+		$('#frequencyInput').val('');
+		$('#firstTrainTime').val('');
+	})
 
-	firebase.database().ref().push({
-		trainName: trainName,
-		destination: destination,
-		firstTrainTime: firstTrainTime,
-		frequency: frequency
-	});
-	$('#trainNameInput').val('');
-	$('#destinationInput').val('');
-	$('#frequencyInput').val('');
-	$('#firstTrainTime').val('');
-})
+	firebase.database().ref().on('child_added', function(snap){
 
-firebase.database().ref().on('child_added', function(snap){
-
-	$('tbody').append(
-		'</tr><tr><td>'+snap.val().trainName+'</td>'+
-		'<td>'+snap.val().destination+'</td>'+
-		'<td>'+snap.val().frequency+'</td></tr>');
-
-	snap.trainName;
-	snap.destination;
-	snap.firstTrainTime;
-	snap.frequency;
-
-	console.log(snap);
+		$('tbody').append(
+			'<tr><td>'+snap.val().trainName+'</td>'+
+			'<td>'+snap.val().destination+'</td>'+
+			'<td>'+snap.val().firstTrainTime+'</td>'+
+			'<td>'+snap.val().frequency+'</td>'+
+			'<td>'+nextTrain+'</td>'+
+			'<td>'+tMinutesTillTrain+'</td></tr>'
+		);		
+	})
 })
   
 
